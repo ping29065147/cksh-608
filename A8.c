ee9a0aa49490204251f7df1ad487a95f7b145464
ping29065147 <32012703+ping29065147@users.noreply.github.com>
2019-04-15 16:03:52 +0800

    Update A8.c

--- A8.c
+++ A8.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    int i=5, j=4, a, b;
+    int i, j, a, b;
 
-    for(; i>0; i--, j--){
+    for(i=5, j=4; i>0; i--, j--){
         for(b=j; b>0; b--)
             printf(" ");
         for(a=i; a>0; a--)
