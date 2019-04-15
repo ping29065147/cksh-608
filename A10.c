794e2e4fd74ad25c4aae483ac4748b422e101563
ping29065147 <32012703+ping29065147@users.noreply.github.com>
2019-04-15 16:11:18 +0800

    Update A10.c

--- A10.c
+++ A10.c
@@ -7,7 +7,7 @@ int main()
 
     for(i=2, j=8; i<11; i+=2, j-=2){
         for(b=j; b>0; b--)
-        printf(" ");
+            printf(" ");
         for(a=i; a>0; a--)
             printf("*");
         printf("\n");
