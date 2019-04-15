5d68667ce1db29ea596fd425947e9018d23227d0
ping29065147 <32012703+ping29065147@users.noreply.github.com>
2019-04-15 15:50:52 +0800

    Update A2.c

--- A2.c
+++ A2.c
@@ -3,13 +3,11 @@
 
 int main()
 {
-    int i=1, j=4, a, b;
+    int i=1, a;
 
-    for(; i<6; i++, j--){
+    for(; i<6; i++){
         for(a=i; a>0; a--)
             printf("*");
-        for(b=j; b>0; b--)
-            printf(" ");
         printf("\n");
     }
 
